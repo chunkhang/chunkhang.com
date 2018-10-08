0fdefde26d1713219101787c6efd198716ce966d
Marcus Mu <chunkhang@gmail.com>
2018-10-08 13:48:03 +0800

    Remove print button

--- _assets/js/scripts.js
+++ _assets/js/scripts.js
@@ -92,19 +92,4 @@
     });
   }
 
-  // Print button
-  const printButton = document.getElementById('js-print');
-  if (printButton !== null) {
-    const resume = document.getElementById('js-resume');
-    printButton.addEventListener('click', function() {
-      window.print();
-    });
-    resume.addEventListener('mouseenter', function() {
-      printButton.classList.add('visible');
-    });
-    resume.addEventListener('mouseleave', function() {
-      printButton.classList.remove('visible');
-    });
-  }
-
 })();
